856f9e0914d943e9536fe77d61c7242d4691d23a
brayanranda <brpenaranda@gmail.com>
2024-02-29 17:22:55 -0500

    fix footer logo

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import { Link, useLocation } from "react-router-dom"
 import { SocialMedia } from "./SocialMedia"
 import { classCircle } from "@/utilities/const"
 import DevelopmentTeam from "@/components/ui/DevelopmentTeam"
+import ImgSlider4 from '../../../assets/images/logo_4.png'
 
 export const Footer = () => {
     const { pathname } = useLocation();
@@ -11,7 +12,9 @@ export const Footer = () => {
             ? <footer className="w-full bg-black py-20 relative z-50 overflow-hidden">
                 <div className="w-11/12 md:w-9/12 mx-auto space-y-28 relative z-10">
                     <div className="flex items-center justify-between">
-                        <p className="text-3xl font-bold text-white">Project Seed</p>
+                        <Link to="/">
+                            <img className='w-52' src={ImgSlider4} alt="logo" />
+                        </Link>
                         <DevelopmentTeam />
                     </div>
                     <div className="flex flex-col md:flex-row md:items-center justify-between gap-y-10">
