ce2492270aca79b0721019e3738b60556e6bd576
brayanranda <brpenaranda@gmail.com>
2024-04-24 18:56:19 -0500

    Fix interface binary search and avl tree

--- src/pages/binary-trees/avl-tree/index.jsx
+++ src/pages/binary-trees/avl-tree/index.jsx
@@ -1,28 +1,39 @@
-import Download from "../../../components/ui/Download/Download";
-
+import Aside from "@/components/ui/Aside";
+import Background from "@/components/ui/Background";
 import Section from "@/components/ui/Section";
+import { useEffect, useState } from "react";
+import AvlTreeDownloads from "./AvlTreeDownloads";
+import { constAvlTree } from "@/utilities/aside/arboles_binarios/avlTree";
 
 export default function AVLTree () {
-    const data = [
-        {
-            title: "Material de Árboles AVL",
-            url: "/contenido/recurso/ARBOLAVL.zip",
-        },
-        {
-            title: "Componente SEED - UFPS",
-            url: "/contenido/componente/SEED_UFPS.zip",
-        },
-        {
-            title: "Simulador para Árboles AVL",
-            url: "/contenido/JARS/SimArbolAVL.zip",
-        },
-    ]
+    const [asideAvlTree, setAsideAvlTree] = useState(constAvlTree);
 
+    useEffect(() => {
+        setAsideAvlTree(constAvlTree)
+    }, [constAvlTree]);
+
+    const [viewTypeComponent, setViewTypeComponent] = useState("arb");
+    const viewComponents = {
+        arb: <Section url="/markdown/avl-tree/description.md" first={true}/>,
+        balA: <Section url="/markdown/avl-tree/balancer.md" first={true}/>,
+        eliD: <Section url="/markdown/avl-tree/eliminar.md" first={true} />,
+        impl: <Section url="/markdown/avl-tree/implementation.md" first={true} last={true}/>,
+    }
+    
     return(
         <main className="bg-seed text-white">
-            <Section url="/markdown/avl-tree/description.md" first={true}/>
-            <Section url="/markdown/avl-tree/implementation.md" last={true}/>
-            <Download data={data} />
+            <Background first={true} last={true} startLeft={false}/>
+            <div className="flex gap-2 relative">
+                <Aside
+                    data={asideAvlTree}
+                    setData={setAsideAvlTree}
+                    setViewTypeComponent={setViewTypeComponent}
+                />
+                <div className="w-10/12">
+                    {viewTypeComponent in viewComponents && viewComponents[viewTypeComponent]}
+                    <AvlTreeDownloads />
+                </div>
+            </div>
         </main>
     );
 }
