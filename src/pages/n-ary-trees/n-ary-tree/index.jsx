4761ccfc5c830a65651aaddc435f2d725869f1cd
brayanranda <brpenaranda@gmail.com>
2024-05-01 21:43:02 -0500

    Interface 123, b, b+ tree

--- src/pages/n-ary-trees/n-ary-tree/index.jsx
+++ src/pages/n-ary-trees/n-ary-tree/index.jsx
@@ -1,28 +1,38 @@
-import Download from "@/components/ui/Download/Download";
-
+import React, { useState, useEffect } from 'react';
 import Section from "@/components/ui/Section";
+import Background from '@/components/ui/Background';
+import Aside from '@/components/ui/Aside';
+import NaryDownloads from './NaryDownloads';
+import { consEnearioTree } from '@/utilities/aside/arboles_enearios/arbol_eneario';
 
 export default function NAryTree () {
-    const data = [
-        {
-            title: "Material Arbol Eneario",
-            url: "/contenido/recurso/ARBOLENEARIO.zip",
-        },
-        {
-            title: "Componente SEED - UFPS",
-            url: "/contenido/componente/SEED_UFPS.zip",
-        },
-        {
-            title: "Simulador para ArbolEneario",
-            url: "/contenido/JARS/SimArbolEnearioGeneral.zip",
-        },
-    ]
+    const [naryTree, setNaryTree] = useState(consEnearioTree);
+
+    useEffect(() => {
+        setNaryTree(consEnearioTree)
+    }, [consEnearioTree]);
+
+    const [viewTypeComponent, setViewTypeComponent] = useState("arb");
+    const viewComponents = {
+        arb: <Section url="/markdown/n-ary-tree/description.md" first={true}/>,
+        reco: <Section url="/markdown/n-ary-tree/recorrido.md" first={true}/>,
+        impl: <Section url="/markdown/n-ary-tree/implementation.md" first={true} last={true}/>,
+    }
 
     return(
         <main className="bg-seed text-white">
-            <Section url="/markdown/n-ary-tree/description.md" first={true}/>
-            <Section url="/markdown/n-ary-tree/implementation.md" last={true}/>
-            <Download data={data} />
+            <Background first={true} last={true} startLeft={false}/>
+            <div className="flex gap-2 relative">
+                <Aside
+                    data={naryTree}
+                    setData={setNaryTree}
+                    setViewTypeComponent={setViewTypeComponent}
+                />
+                <div className="w-10/12">
+                    {viewTypeComponent in viewComponents && viewComponents[viewTypeComponent]}
+                    <NaryDownloads />
+                </div>
+            </div>
         </main>
-    );
+    )
 }
---
+++ src/utilities/aside/grafos.js
@@ -0,0 +1,33 @@
+export const consGrafos = [
+    {
+        key: "arb",
+        title: "Teoría de Grafos",
+        state: true,
+    },
+    {
+        key: "ope",
+        title: "Operaciones",
+        state: false,
+    },
+    {
+        key: "impl",
+        title: "Implementación",
+        state: false,
+    },
+]
+// Teoría de Grafos
+// Representaciones
+// Grafos Conexos
+// Terminologias
+// Familias distinguidas de Grafos Simples
+// Grafos Bipartitos
+// Representación de los Grafos
+// Isomorfismo de Grafos
+// Ruta o Camino de Longitud "n"
+// Circuitos
+
+// Grafos Ponderados y Algoritmos
+// Algoritmo de Dijkstra
+
+// Algoritmos
+// Implementación
