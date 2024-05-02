a24c20e89c877b934dc18d12d07a1e0637735246
brayanranda <brpenaranda@gmail.com>
2024-05-02 09:47:51 -0500

    Fix interface graph

--- src/pages/graphic-schema-theory/graph/index.jsx
+++ src/pages/graphic-schema-theory/graph/index.jsx
@@ -1,28 +1,49 @@
-import Download from "@/components/ui/Download/Download";
-
+import React, { useState, useEffect } from 'react';
+import Aside from "@/components/ui/Aside";
+import Background from "@/components/ui/Background";
 import Section from "@/components/ui/Section";
+import { consGrafos } from "@/utilities/aside/grafos";
+import GraphDownloads from "./GraphDownloads";
 
 export default function Graph () {
-    const data = [
-        {
-            title: "Material de Teoría de Grafos",
-            url: "/contenido/recurso/GRAFOS.zip",
-        },
-        {
-            title: "Componente SEED - UFPS",
-            url: "/contenido/componente/SEED_UFPS.zip",
-        },
-        {
-            title: "Simulador para Grafos",
-            url: "/contenido/JARS/SimGrafoND.zip",
-        },
-    ]
+    const [grafos, setGrafos] = useState(consGrafos);
+
+    useEffect(() => {
+        setGrafos(consGrafos)
+    }, [consGrafos]);
+
+    const [viewTypeComponent, setViewTypeComponent] = useState("teo");
+    const viewComponents = {
+        teo: <Section url="/markdown/graph/description.md" first={true} />,
+        rep: <Section url="/markdown/graph/representaciones.md" first={true} />,
+        grafCon: <Section url="/markdown/graph/grafosConexos.md" first={true} />,
+        term: <Section url="/markdown/graph/terminologias.md" first={true} />,
+        fam: <Section url="/markdown/graph/familias.md" first={true} />,
+        grafBip: <Section url="/markdown/graph/grafosBipartitos.md" first={true} />,
+        repGra: <Section url="/markdown/graph/representacionGrafos.md" first={true} />,
+        iso: <Section url="/markdown/graph/isomorfismoGrafos.md" first={true} />,
+        rut: <Section url="/markdown/graph/rutaCamino.md" first={true} />,
+        cir: <Section url="/markdown/graph/circuitos.md" first={true} />,
+        grafPon: <Section url="/markdown/graph/grafosPonderados.md" first={true} />,
+        algDij: <Section url="/markdown/graph/algoritmoDijkstra.md" first={true} />,
+        alg: <Section url="/markdown/graph/algoritmos.md" first={true} />,
+        impl: <Section url="/markdown/graph/implementation.md" first={true} last={true} />,
+    }
 
     return(
         <main className="bg-seed text-white">
-            <Section url="/markdown/graph/description.md" first={true}/>
-            <Section url="/markdown/graph/implementation.md" last={true}/>
-            <Download data={data} />
+            <Background first={true} startLeft={false}/>
+            <div className="flex gap-2 relative">
+                <Aside
+                    data={grafos}
+                    setData={setGrafos}
+                    setViewTypeComponent={setViewTypeComponent}
+                />
+                <div className="w-10/12">
+                    {viewTypeComponent in viewComponents && viewComponents[viewTypeComponent]}
+                    <GraphDownloads />
+                </div>
+            </div>
         </main>
     );
 }
--- src/utilities/aside/grafos.js
+++ src/utilities/aside/grafos.js
@@ -1,12 +1,67 @@
 export const consGrafos = [
     {
-        key: "arb",
+        key: "teo",
         title: "Teoría de Grafos",
         state: true,
     },
     {
-        key: "ope",
-        title: "Operaciones",
+        key: "rep",
+        title: "Representaciones",
+        state: false,
+    },
+    {
+        key: "grafCon",
+        title: "Grafos Conexos",
+        state: false,
+    },
+    {
+        key: "term",
+        title: "Terminologias",
+        state: false,
+    },
+    {
+        key: "fam",
+        title: "Familias distinguidas de Grafos Simples",
+        state: false,
+    },
+    {
+        key: "grafBip",
+        title: "Grafos Bipartitos",
+        state: false,
+    },
+    {
+        key: "repGra",
+        title: "Representación de los Grafos",
+        state: false,
+    },
+    {
+        key: "iso",
+        title: "Isomorfismo de Grafos",
+        state: false,
+    },
+    {
+        key: "rut",
+        title: 'Ruta o Camino de Longitud "n"',
+        state: false,
+    },
+    {
+        key: "cir",
+        title: "Circuitos",
+        state: false,
+    },
+    {
+        key: "grafPon",
+        title: "Grafos Ponderados y Algoritmos",
+        state: false,
+    },
+    {
+        key: "algDij",
+        title: "Algoritmo de Dijkstra",
+        state: false,
+    },
+    {
+        key: "alg",
+        title: "Algoritmos",
         state: false,
     },
     {
@@ -15,19 +70,3 @@ export const consGrafos = [
         state: false,
     },
 ]
-// Teoría de Grafos
-// Representaciones
-// Grafos Conexos
-// Terminologias
-// Familias distinguidas de Grafos Simples
-// Grafos Bipartitos
-// Representación de los Grafos
-// Isomorfismo de Grafos
-// Ruta o Camino de Longitud "n"
-// Circuitos
-
-// Grafos Ponderados y Algoritmos
-// Algoritmo de Dijkstra
-
-// Algoritmos
-// Implementación
