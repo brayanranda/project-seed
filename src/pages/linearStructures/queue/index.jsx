1a9493e23c4489eb8d137a7adc0e6f2e93664b1c
brayanismaelpr <brayanismaelpr@ufps.edu.co>
2024-06-03 16:06:16 -0500

    Fix modal y run code 3.0

--- src/pages/linearStructures/queue/index.jsx
+++ src/pages/linearStructures/queue/index.jsx
@@ -4,21 +4,39 @@ import { getAnalisis_COLAS, getAnalisis_COLASP } from "@/store/services/servicio
 import QueueDownloads from "./QueueDownloads";
 import { constColas } from "@/utilities/aside/estructuras_lineales/colas";
 import { useEffect, useState } from "react";
-import FrameCode from "../../../components/ui/FrameCode";
 import Template from "@/components/Layout/Template";
+import ModalPythonTutor from "@/components/ui/ModalPythonTutor";
 
 export default function Queue() {
     const [asideQueue, setAsideQueue] = useState(constColas);
+    const [modal, setModal] = useState(false);
+    const [stringModal, setStringModal] = useState("");
 
     useEffect(() => {
         setAsideQueue(constColas)
     }, [constColas]);
 
+    const showModal = () => {
+        setModal(!modal)
+    }
+    const typeModal = {
+        cola: <ModalPythonTutor modal={modal} showModal={showModal} url="/code-runner/colas/Cola.java" />,
+        colaP: <ModalPythonTutor modal={modal} showModal={showModal} url="/code-runner/colas/ColaP.java" />,
+    }
+
     const [viewTypeComponent, setViewTypeComponent] = useState("col");
     const viewComponents = {
         col: <>
-            <Section url="/markdown/queue/description.md" first={true} />
-            <FrameCode url="/code-runner/colas/Cola.java" />
+            <Section
+                showBtnModal={true}
+                showModal={showModal}
+                titleBtn="Cola"
+                setStringModal={setStringModal}
+                typeModal="cola"
+
+                url="/markdown/queue/description.md"
+                first={true}
+            />
         </>,
         implementacionCola: <Section url="/markdown/queue/implementation.md" first={true} />,
         costoComplejidadCola: <Analisis
@@ -28,8 +46,16 @@ export default function Queue() {
             sub_title="Costo Operacional y Complejidad de"
         />,
         colp: <>
-            <Section url="/markdown/queue/description-priority.md" first={true} startLeft={true} />
-            {/* <FrameCode url="/code-runner/colas/ColaP.java" /> */}
+            <Section
+                showBtnModal={true}
+                showModal={showModal}
+                titleBtn="Cola P"
+                setStringModal={setStringModal}
+                typeModal="colaP"
+
+                url="/markdown/queue/description-priority.md"
+                first={true} startLeft={true}
+            />
         </>,
         implementacionColaPrioridad: <Section url="/markdown/queue/implementation-priority.md" first={true} />,
         costoComplejidadColaPrioridad: <Analisis
@@ -44,6 +70,8 @@ export default function Queue() {
         <Template
             data={asideQueue}
             setData={setAsideQueue}
+            typeModal={typeModal}
+            stringModal={stringModal}
             viewComponents={viewComponents}
             viewTypeComponent={viewTypeComponent}
             setViewTypeComponent={setViewTypeComponent}
